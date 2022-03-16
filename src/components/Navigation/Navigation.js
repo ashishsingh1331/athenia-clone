a366bb6415dca5ee39654f8e6f335eaffd6ea950
ashishdrupal <ashishsingh1331@gmail.com>
2022-03-16 14:29:05 +0530

    1) Addeed hero icons 2) Themed navigaiton.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Hamburger from "../Header/Hamburger";
 import { useState, useCallback, useEffect } from "react";
+import { PhoneIcon } from '@heroicons/react/solid'
+import { XIcon } from '@heroicons/react/outline'
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,6 +10,10 @@ const Navigation = () => {
     e.preventDefault();
     setIsOpen((isOpen) => !isOpen);
   };
+
+  const closeDrawer = (e) => {
+    setIsOpen(false);
+  }
 
   const escFunction = useCallback((event) => {
     if (event.key === "Escape") {
@@ -31,13 +37,32 @@ const Navigation = () => {
         <div className="w-7 h-[4px] bg-white"></div>
       </a>
       <ul
-        className={` absolute right-0 top-0 h-screen w-screen md:w-[50%] flex flex-col items-center justify-center translate-x-[100%] opacity-0 transition-all bg-light-pink duration-700 menu ${
-          isOpen ? " translate-x-0 opacity-100" : ""
+        className={` absolute right-0 top-0 h-screen w-screen md:w-[50%] flex flex-col items-center justify-center translate-x-[100%]  transition-all bg-light-pink duration-700 menu ${
+          isOpen ? " translate-x-0 " : ""
         } `}
       >
-        <li>
-          <a href="#">Home</a>
+        <li className=" absolute top-[10px] right-[10px] hover:bg-pink-200 hover:cursor-pointer ">
+            <a onClick={closeDrawer}><XIcon className="w-6 h-6"/></a>
+        </li>  
+        <li className="mb-3  w-full text-center">
+          <a className=" uppercase text-sm md:text-xl lg:text-3xl  text-dark-gray tracking-widest hover:underline " href="#">Bridals</a>
         </li>
+        <li className="mb-3 w-full text-center">
+          <a className=" uppercase text-sm md:text-xl lg:text-3xl  text-dark-gray tracking-widest hover:underline" href="#">Nail Extension</a>
+        </li>
+        <li className="mb-3 w-full text-center">
+          <a className=" uppercase text-sm md:text-xl lg:text-3xl  text-dark-gray tracking-widest hover:underline" href="#">Hair Styles</a>
+        </li>
+        <li className="mb-3 w-full text-center">
+          <a className=" uppercase text-sm md:text-xl lg:text-3xl  text-dark-gray tracking-widest hover:underline" href="#">Skin Services</a>
+        </li>
+        <li className="mb-3 w-full text-center">
+          <a className=" uppercase text-sm md:text-xl lg:text-3xl  text-dark-gray tracking-widest hover:underline" href="#">Eyelash extension</a>
+        </li>
+        <li className="mb-3 w-full text-center">
+        <a className="flex items-center justify-center text-sm md:text-xl lg:text-3xl" href="tel:+9716666666"><PhoneIcon className=" h-4 w-4 md:h-5 md:w-5 lg:h-6 lg:w-6"/><p>+91 9717777776</p></a>
+        </li>
+        
       </ul>
     </nav>
   );
